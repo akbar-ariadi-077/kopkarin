9d8f5296862442c392b265bb9978282da9d31b6a
Akbar Ariadi <akbar.ariadi.077@gmail.com>
2023-09-03 08:22:55 +0700

    fix total aneka usaha

--- application/views/usaha/transaksi.php
+++ application/views/usaha/transaksi.php
@@ -19,27 +19,39 @@
                         <br><br>
                         <div class="row">
                             <div class="col-lg-12 col-md-12">
-                                <h3 style="text-align: center; font-weight: bold;"><u>DATA TRANSAKSI <?= $customer_nam; ?></u></h3>
+                                <h3 style="text-align: center; font-weight: bold;"><u>DATA TRANSAKSI</u></h3>
                             </div>
                         </div>
                         <br>
-                        <table id="zero-config" class="table dt-table-hover" style="width:100%">
+                        <table id="zero-config" class="table dt-table-hover display compact" style="width:100%">
                             <thead>
+                                <!--<tr>-->
+                                <!--    <th rowspan="2" align="center">No.</th>-->
+                                <!--    <th colspan="2" align="center">DP</th>-->
+                                <!--    <th colspan="4" align="center">Sales</th>-->
+                                <!--    <th rowspan="2" align="center">Kurang/Lebih</th>-->
+                                <!--    <th rowspan="2" align="center">Keterangan</th>-->
+                                <!--</tr>-->
+                                <!--<tr>-->
+                                <!--    <th>Tanggal</th>-->
+                                <!--    <th>Jumlah</th>-->
+                                <!--    <th>Tanggal</th>-->
+                                <!--    <th>Volume</th>-->
+                                <!--    <th>Price</th>-->
+                                <!--    <th>Jumlah</th>-->
+                                <!--</tr>-->
                                 <tr>
-                                    <th>No.</th>
-                                    <th>Tanggal</th>
-                                    <th>Jumlah</th>
-                                    <th>Tanggal</th>
-                                    <th>Volume</th>
-                                    <th>Price</th>
-                                    <th>Jumlah</th>
-                                    <th>Kurang/Lebih</th>
-                                    <th>Keterangan</th>
+                                    <th  style="text-align: center;">No.</th>
+                                    <th colspan="2" style="text-align: center;">DP</th>
+                                    <th colspan="4" style="text-align: center;">Sales</th>
+                                    <th style="text-align: center;">Kurang/Lebih</th>
+                                    <th style="text-align: center;">Keterangan</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <?php $z = 0; ?>
+                                <?php $z = 0; $total = 0; $total_all = 0; ?>
                                 <?php foreach ($transaksi as $y): ?>
+                                    <?= ($y['jenis_transaksi'] == 'DP Masuk') ? $total += $y['jumlah'] : $total -= $y['jumlah']; ?>
                                     <tr>
                                         <td>
                                             <?= $z += 1; ?>
@@ -62,8 +74,10 @@
                                         <td align="right">
                                         <?= ($y['jenis_transaksi'] == 'Penjualan') ? number_format($y['jumlah']) : '&nbsp;'; ?>
                                         </td>
-                                        <td>
-                                            &nbsp;
+                                        <td align="right">
+                                            <?php $total_all += $total; $totall = $total_all; ?>
+                                            <!--<?= number_format($total_all); ?>-->
+                                            <?= ($totall < 0) ? '('.number_format(abs($totall)).')' : number_format($totall); ?>
                                         </td>
                                         <td>
                                             <?= $y['keterangan_transaksi']; ?>
@@ -72,6 +86,11 @@
                                 <?php endforeach; ?>
                             </tbody>
                         </table>
+                        <br>
+                        <div class="col-lg-12 col-md-12">
+                            <h3 style="text-align: center; font-weight: bold;"><u><?= $customer_nam; ?></u></h3>
+                        </div>
+                        <br><br>
                     </div>
                 </div>
             </div>
